37b2bdec4f575fb5f94d63e0021173d1baf1f3fa
meng.y <meng.y@Ctrip.com>
2015-08-24 15:02:14 +0800

    fixed bug

--- arrow.js
+++ arrow.js
@@ -10,7 +10,7 @@ arrow.prototype = {
   draw:function(context){
     context.save();
     context.translate(this.x,this.y);
-    context.rotation(this.rotation);
+    context.rotate(this.rotation);
     context.strokeStyle = this.strokeStyle;
     context.fillStyle = this.fillcolor;
     context.lineWidth = 3;
